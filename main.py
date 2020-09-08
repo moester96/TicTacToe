27309a75bd27b127a90cbb2b1797efd7c528d7d1
moester96 <moester9696@gmail.com>
2020-09-08 16:04:41 -0500

    Debugging Game Functionality

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from tictactoe import TicTacToe
 
 def main():
     tic = TicTacToe()
+    tic.chooseLetter()
     tic.drawBoard()
+
 
 if __name__ == '__main__':
     main()
--- tictactoe.py
+++ tictactoe.py
@@ -10,14 +10,15 @@ class TicTacToe:
     def __init__(self):
         self.turtle = turtle.Turtle()
         self.screen = turtle.Screen()
+        self.board = [['' for i in range(3)] for j in range(3)]
+        self.next_turn = ''
+        self.num_turns = 0
         self.square = 0
+        self.isWinner = False
 
-    def getMouseClick(self, x, y):
-        column = (x + 300) // 200
-        row = (-y + 300) // 200
-        square = column + row*3
-        square = int(square)
-        self.square = square
+    def chooseLetter(self):
+        letter = input('Please select your letter of choice: X or O\n')
+        self.next_turn = letter.lower()
 
     def drawX(self):
         square = str(self.square)
@@ -25,6 +26,17 @@ class TicTacToe:
 
         self.turtle.penup()
         self.turtle.goto(mid_point)
+        self.turtle.pendown()
+        self.turtle.setheading(135)
+
+        for i in range(2):
+            self.turtle.forward(90)
+            self.turtle.backward(180)
+            self.turtle.goto(mid_point)
+            self.turtle.left(270)
+
+        self.turtle.penup()
+        self.turtle.home()
 
     def drawO(self):
         square = str(self.square)
@@ -34,6 +46,79 @@ class TicTacToe:
         self.turtle.goto(mid_point[0], mid_point[1] - 80)
         self.turtle.pendown()
         self.turtle.circle(80)
+
+        self.turtle.penup()
+
+    def checkWinner(self):
+        first_diagonal = []
+        second_diagonal = []
+
+        # Checks rows
+        for i in range(3):
+            if set(self.board[i]) == 1:
+                print('Winner')
+                return True
+
+        # Checks columns
+        for i in range(3):
+            if set(self.board[0][i]) == 1:
+                print('Winner')
+                return True
+
+        # Checks first diagonal
+        for i in range(3):
+            first_diagonal.append(self.board[i][i])
+
+        if set(first_diagonal) == 1:
+            print('Winner')
+            return True
+        else:
+            first_diagonal = []
+
+        # Checks second diagonal
+        for i in range(3):
+            if i != 1:
+                second_diagonal.append(self.board[i][2])
+            else:
+                second_diagonal.append(self.board[i][i])
+
+        if set(second_diagonal) == 1:
+            print('Winner')
+            return True
+        else:
+            first_diagonal = []
+
+        print('No winner')
+        return False
+
+    def checkDraw(self):
+        if self.checkWinner() == False:
+            print('Draw')
+
+    def getMouseClick(self, x, y):
+        column = (x + 300) // 200
+        row = (-y + 300) // 200
+        square = column + row*3
+        self.square = int(square)
+
+        if self.board[self.square] == '' and self.next_turn == 'x':
+            self.drawX()
+            self.board[self.square] = self.next_turn
+            self.next_turn = 'o'
+            self.num_turns += 1
+            if self.num_turns >= 5:
+                self.isWinner = self.checkWinner()
+
+        elif self.board[self.square] == '' and self.next_turn == 'o':
+            self.drawO()
+            self.board[self.square] = self.next_turn
+            self.next_turn = 'x'
+            self.num_turns += 1
+            if self.num_turns >= 5:
+                self.isWinner = self.checkWinner()
+
+        if self.num_turns == 9:
+            self.checkDraw()
 
     def drawBoard(self):
         self.screen.title("TicTacToe")
